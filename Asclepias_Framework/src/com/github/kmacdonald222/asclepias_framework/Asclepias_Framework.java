c0b4fdc10abc59864ec237bcc01627994a5e0a92
KMacDonald222 <keeganm742@gmail.com>
2025-05-13 10:22:27 -0300

    Added initial Eclipse projects

---
+++ Asclepias_Framework/src/com/github/kmacdonald222/asclepias_framework/Asclepias_Framework.java
@@ -0,0 +1,7 @@
+package com.github.kmacdonald222.asclepias_framework;
+
+public class Asclepias_Framework {
+	public void print(String input) {
+		System.out.println("Print: \"" + input + "\"");
+	}
+}
---
+++ Asclepias_Test/src/com/github/kmacdonald222/asclepias_test/Asclepias_Test.java
@@ -0,0 +1,10 @@
+package com.github.kmacdonald222.asclepias_test;
+
+import com.github.kmacdonald222.asclepias_framework.*;
+
+public class Asclepias_Test {
+	public static void main(String[] args) {
+		Asclepias_Framework af = new Asclepias_Framework();
+		af.print("Hello World!");
+	}
+}
