1379ab35fe454d3fdea15120e18209b8ee84dc39
KMacDonald222 <keeganm742@gmail.com>
2025-05-13 10:24:39 -0300

    Removed project testing files

--- Asclepias_Framework/src/com/github/kmacdonald222/asclepias_framework/Asclepias_Framework.java
+++
@@ -1,7 +0,0 @@
-package com.github.kmacdonald222.asclepias_framework;
-
-public class Asclepias_Framework {
-	public void print(String input) {
-		System.out.println("Print: \"" + input + "\"");
-	}
-}
